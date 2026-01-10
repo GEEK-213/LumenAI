be4aec0ea48f42813c52687055b45fd9de2cdbce
ChethanBadiger <chethanbadiger245@gmail.com>
2026-01-10 21:51:17 +0530

    signup_and_login part 1

--- LumenAi/server/main.py
+++ LumenAi/server/main.py
@@ -1,7 +1,30 @@
+# from fastapi import FastAPI
+# from pydantic import BaseModel
+
+# app = FastAPI()
+
+# class UserCreate(BaseModel):
+#     name: str
+#     email: str
+#     password: str
+
+# @app.post('/signup')
+# def signup_user(user: UserCreate):
+#     #request data
+#     print(user.name)
+#     print(user.email)
+#     print(user.password)
+#     pass
+
+
 from fastapi import FastAPI
+from models.base import Base
+from routes import auth, song
+from database import engine
 
 app = FastAPI()
 
-@app.get('/')
-def test():
-    return 'Hello world'
+app.include_router(auth.router, prefix='/auth')
+app.include_router(song.router, prefix='/song')
+
+Base.metadata.create_all(engine)
