6b2d083ccbb2258b3f88df49f1828ebca498fc97
Gauresh Gaude <gaureshgaude2@gmail.com>
2026-01-10 23:12:28 +0530

    feat:  added ProfilePage with navigation

--- LumenAi/app/lib/main.dart
+++ LumenAi/app/lib/main.dart
@@ -1,9 +1,8 @@
-import 'package:app/pages/login.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'pages/login.dart';
-import 'pages/register.dart';
-// import 'pages/homePage.dart';
+// import 'pages/login.dart';
+// import 'pages/register.dart';
+import 'pages/homePage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -26,7 +25,7 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: const Color(0xFF080E22),
         primaryColor: const Color(0xFF1E88E5),
       ),
-      home: const LoginPage(),
+      home: const HomePage(),
     );
   }
 }
